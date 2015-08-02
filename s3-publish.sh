3bc93d21faa99c336d3f92fc33ad9da6fc1299a1
Paulo Barcelos <paulo@azucrina.org>
2015-08-02 18:05:46 +0300

    Added S3 publish script

---
+++ s3-publish.sh
@@ -0,0 +1,16 @@
+gulp
+rm -r s3_publish
+cp -r dist s3_publish
+
+rm -r s3_publish/bower_components
+mkdir -p s3_publish/bower_components/webcomponentsjs
+cp dist/bower_components/webcomponentsjs/webcomponents-lite.min.js \
+s3_publish/bower_components/webcomponentsjs/webcomponents-lite.min.js
+
+rm -r s3_publish/elements
+mkdir -p s3_publish/elements
+cp  dist/elements/elements.vulcanized.html \
+s3_publish/elements/elements.vulcanized.html
+
+aws s3 sync s3_publish s3://code.quirkbot.com/program --exclude *.DS_Store
+rm -r s3_publish
