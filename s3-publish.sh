89142ff9c8ba645308267921e081d4d31578ea1e
Paulo Barcelos <paulo@azucrina.org>
2015-09-20 21:07:37 +0200

    Small fixes

--- s3-publish.sh
+++ s3-publish.sh
@@ -1,4 +1,11 @@
 gulp static
 
 aws s3 sync static/_static s3://code.quirkbot.com/_static --delete --exclude *.DS_Store
+
+aws s3 sync static/confirm s3://code.quirkbot.com/confirm --delete --exclude *.DS_Store
 aws s3 sync static/program s3://code.quirkbot.com/program --delete --exclude *.DS_Store
+aws s3 sync static/register s3://code.quirkbot.com/register --delete --exclude *.DS_Store
+aws s3 sync static/user s3://code.quirkbot.com/user --delete --exclude *.DS_Store
+
+aws s3 cp static/index.html s3://code.quirkbot.com/index.html
+aws s3 cp static/404.html s3://code.quirkbot.com/404.html
