afc2af9b5fe458f85b9eff252f7adaccdcc1b79d
Paulo Barcelos <paulo@azucrina.org>
2015-11-02 18:44:38 +0100

    Added new profile to s3 publish

--- s3-publish.sh
+++ s3-publish.sh
@@ -1,35 +1,42 @@
 gulp gzip-static
 
 aws s3 sync static s3://code.quirkbot.com \
+--profile quirkbot \
 --content-encoding="gzip" \
 --content-type="application/javascript" \
 --cache-control "max-age 86400" \
 --exclude '*' --include '*.js' &&\
 aws s3 sync static s3://code.quirkbot.com \
+--profile quirkbot \
 --content-encoding="gzip" \
 --content-type="text/css" \
 --cache-control "max-age 86400" \
 --exclude '*' --include '*.css' &&\
 aws s3 sync static s3://code.quirkbot.com \
+--profile quirkbot \
 --content-encoding="gzip" \
 --content-type="text/html" \
 --cache-control "max-age 86400" \
 --exclude '*' --include '*.html' &&\
 aws s3 sync static s3://code.quirkbot.com \
+--profile quirkbot \
 --content-encoding="gzip" \
 --content-type="application/font-woff2" \
 --cache-control "max-age 86400" \
 --exclude '*' --include '*.woff2' &&\
 aws s3 sync static s3://code.quirkbot.com \
+--profile quirkbot \
 --content-encoding="gzip" \
 --content-type="application/json" \
 --cache-control "max-age 86400" \
 --exclude '*' --include '*.json' &&\
 aws s3 sync static s3://code.quirkbot.com \
+--profile quirkbot \
 --content-encoding="gzip" \
 --content-type="image/svg+xml" \
 --cache-control "max-age 86400" \
 --exclude '*' --include '*.svg' &&\
 aws s3 sync static s3://code.quirkbot.com \
+--profile quirkbot \
 --cache-control "max-age 86400" \
 --exclude '*.js' --exclude '*.css' --exclude '*.html' --exclude '*.woff2' --exclude '*.svg' --exclude '*.json' --exclude '*.DS_Store'
