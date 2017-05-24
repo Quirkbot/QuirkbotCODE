75e5b66132311cc93facce5f19dca9bb735e6d8d
Paulo Barcelos <paulo@azucrina.org>
2017-05-24 16:42:29 +0200

    moved to yarn

--- prepublish.sh
+++ prepublish.sh
@@ -7,28 +7,28 @@ rm -r dist
 mkdir dist
 
 # Created development environment
-#npm run gulp -- build:gzip --environment=development
+#yarn run gulp -- build:gzip --environment=development
 #mkdir dist/development
 #mv dist_dev dist/development/dist_dev
 #mv dist_polymer dist/development/dist_polymer
 #mv dist_gzip dist/development/dist_gzip
 
 # Created stage environment
-#npm run gulp -- build:gzip --environment=stage
+#yarn run gulp -- build:gzip --environment=stage
 #mkdir dist/stage
 #mv dist_dev dist/stage/dist_dev
 #mv dist_polymer dist/stage/dist_polymer
 #mv dist_gzip dist/stage/dist_gzip
 
 # Created production environment
-npm run gulp -- build:gzip --environment=production
+yarn run gulp -- build:gzip --environment=production
 mkdir dist/production
 #mv dist_dev dist/production/dist_dev
 mv dist_polymer dist/production/dist_polymer
 mv dist_gzip dist/production/dist_gzip
 
 # Created lite environment
-npm run gulp -- build:gzip --environment=lite
+yarn run gulp -- build:gzip --environment=lite
 mkdir dist/lite
 #mv dist_dev dist/lite/dist_dev
 mv dist_polymer dist/lite/dist_polymer
